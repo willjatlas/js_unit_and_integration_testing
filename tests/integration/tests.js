269ac99f1e88cde9e9b361294ecf9a2dbfe35936
William Anderson <willjatlas@googlemail.com>
2021-01-22 16:12:52 +0000

    :can result negative numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -25,4 +25,25 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   });
 
+  it('should chain multiple operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_add')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  });
+
+  it('should deal with negative numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-3')
+  });
+
+
+
+
+
 });
