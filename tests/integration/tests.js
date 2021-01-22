f3fe40f18bb2fc08760898cccf2205b3020e5848
William Anderson <willjatlas@googlemail.com>
2021-01-22 16:19:16 +0000

    outputs large numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -33,13 +33,34 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   });
 
-  it('should deal with negative numbers', function(){
+  it('should output negative numbers', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number6')).click();
     element(by.css('#operator_subtract')).click();
     element(by.css('#number9')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('-3')
+  });
+
+  it('should output decimal numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
+  });
+
+  it('should output large numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_multiply')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10000000000000000')
   });
 
 
